11d69f6a8a948d4aa3c705a143ab05ff0b3c06e7
sabta <alencarsabta@gmail.com>
2023-04-14 11:39:48 -0400

    feat: backend

--- imaginia-backend/src/controllers/users-controller.ts
+++ imaginia-backend/src/controllers/users-controller.ts
@@ -6,15 +6,14 @@ import authenticationService from "../services/authentication-service";
 
 export async function userSignup(req: Request, res: Response) {
   const { email, password } = req.body;
-console.log(req.body, "cadastro")
   try {
     const user = await userService.createUser({ email, password });
+
     return res.status(httpStatus.CREATED).json({
       id: user.id,
       email: user.email,
     });
   } catch (error) {
-    console.log(error, "erro cadastro")
     if (error.name === "DuplicatedEmailError") {
       return res.status(httpStatus.CONFLICT).send({});
     }
@@ -24,12 +23,14 @@ console.log(req.body, "cadastro")
 
 export async function userSignin(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
-
   try {
     const result = await authenticationService.userSignin({ email, password });
 
     return res.status(httpStatus.OK).send(result);
   } catch (error) {
-    return res.status(httpStatus.UNAUTHORIZED).send({});
+    if (error.name === "InvalidCredentialsError") {
+      return res.status(httpStatus.UNAUTHORIZED).send({});
+    }
+    return res.status(httpStatus.BAD_REQUEST).send({});
   }
 }
